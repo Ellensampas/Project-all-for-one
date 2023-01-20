3f0cf24af755eb880536a8be3bdf851fb1f82d6a
ellen Sampaio <ellenssbarross@gmail.com>
2023-01-20 18:27:01 -0300

    req24 - corrigindo erros

--- desafio24.sql
+++ desafio24.sql
@@ -1,1 +1,1 @@
-UPDATE order_details SET discount = 45 WHERE unit_price > 10.0000 AND ( id > 30 AND id < 40);
+UPDATE order_details SET discount = 45 WHERE id BETWEEN 30 AND 40 AND unit_price > 10.000;
